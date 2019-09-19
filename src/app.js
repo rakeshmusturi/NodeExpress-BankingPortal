cc6c6f3aae28cad34bdbb5a3f71839d8280ff878
musturi <rakesh.musturi.ext@nokia.com>
2019-09-19 19:53:50 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
- 
+const fs = require('fs');
+const path = requires('path');
+
+const express = require('express');
+const app = express();
+app.set('views', path.join(_dirname, 'views'));
+app.set('view engine', 'ejs');
