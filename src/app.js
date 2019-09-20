f4a59ded47b10e40d4e425f8b81afd0fb566d75a
musturi <rakesh.musturi.ext@nokia.com>
2019-09-20 19:04:15 +0530

    complete Code

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 const accountData = fs.readFileSync(
   path.join(__dirname, 'json', 'accounts.json'), 'utf8'
@@ -27,6 +28,25 @@ app.get('/savings', (req, res)=> {
 app.get('/credit', (req, res)=> {
   res.render('account', { account : accounts.credit })
 });
+
+app.get('/transfer', (req, res)=>res.render('transfer'));
+app.post('/transfer', (req, res)=> {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.from].balance) + parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res)=> res.render('payment', {account: accounts.credit}));
+app.post('/payment',(req, res)=>{
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.avaliable += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+  res.render('payment', {message: 'Payment Sucessful', account: accounts.credit});
+})
+
 app.get('/checking', (req, res)=> {
   res.render('account', { account : accounts.checking })
 });
